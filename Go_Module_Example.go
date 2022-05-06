de87a9a464148ed7932077cac27f57cc2aba6b80
AnonymousRecess <Jeffkahn@u.boisestate.edu>
2022-05-06 02:07:13 -0600

    formatting

--- Go_Module_Example.go
+++ Go_Module_Example.go
@@ -6,9 +6,13 @@ import (
 )
 
 func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
 	if name == "" {
 		return "", errors.New("empty name")
 	}
+
+	// If a name was received, return a value that embeds the name
+	// in a greeting message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message, nil
 }
